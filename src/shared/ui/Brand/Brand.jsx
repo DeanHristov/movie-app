125be33d4d451a593257d00f86dd1677d242718b
D. Hristov <dean.hristov89@gmail.com>
2019-04-20 13:01:28 +0300

    Updated brand styles

--- src/shared/ui/Brand/Brand.jsx
+++ src/shared/ui/Brand/Brand.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './Brand.scss'
-import {faFilm} from "@fortawesome/free-solid-svg-icons/faFilm";
+import {faVideo} from "@fortawesome/free-solid-svg-icons";
 
 export default class Brand extends Component {
   state = {
@@ -13,7 +13,7 @@ export default class Brand extends Component {
     const {title} = this.state;
     return (
       <div className="brand-container">
-        <FontAwesomeIcon icon={faFilm} size="6x" color={'#2a2b30'}/>
+        <FontAwesomeIcon icon={faVideo} size="4x" color={'#ff0000'}/>
         <h2 className="brand-title text-center text-bold-italic"> {title}</h2>
       </div>
     )
