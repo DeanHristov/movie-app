110ee425fda3747c7063c97eb1a5a14e59f12e26
D. Hristov <dean.hristov89@gmail.com>
2019-04-22 16:27:12 +0300

    Updated UI styles

--- src/routes/Login/LoginContainer.jsx
+++ src/routes/Login/LoginContainer.jsx
@@ -1,14 +1,36 @@
 import React, {Component} from 'react'
+import LogInButton from "./ui/LogInButton/LogInButton";
+import {connect} from "react-redux";
+import { withRouter } from 'react-router-dom'
+
+import {doLogin} from "@store/actions/userActions";
+import auth from '@core/Auth'
 
 import './LoginContainer.scss';
-import LogInButton from "./ui/LogInButton/LogInButton";
 
-export default class LoginContainer extends Component {
+@connect(null, dispatch => ({
+  login: params => dispatch(doLogin(params))
+}))
+
+class LoginContainer extends Component {
+
+  onLogin () {
+    const { history } = this.props;
+
+    auth.login((params) => {
+      this.props.login(params)
+      history.push('/movies')
+    })
+  }
   render() {
     return (
       <div id={'login-container'}>
-        <LogInButton/>
+        <LogInButton onClick={::this.onLogin}/>
       </div>
     )
   }
 }
+
+export default withRouter(LoginContainer)
+
+
